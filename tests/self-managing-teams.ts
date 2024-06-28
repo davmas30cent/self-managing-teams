428dfca42e3349b5810fdcf479b3f2662386192b
Dávid Maslo <d.maslo@tricentis.com>
2024-06-28 16:07:20 +0200

    added team_members account together with it's signature into initialization call
    Signed-off-by: Dávid Maslo <d.maslo@tricentis.com>

--- tests/self-managing-teams.ts
+++ tests/self-managing-teams.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SelfManagingTeams } from "../target/types/self_managing_teams";
+import { Keypair } from "@solana/web3.js"
 
 describe("self-managing-teams", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,17 @@ describe("self-managing-teams", () => {
 
   const program = anchor.workspace.SelfManagingTeams as Program<SelfManagingTeams>;
 
+  const newAccountKp = Keypair.generate();
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+                .initialize()
+                .accounts({
+                  teamMembers: newAccountKp.publicKey,
+                })
+                .signers([newAccountKp])
+                .rpc();
     console.log("Your transaction signature", tx);
   });
 });
